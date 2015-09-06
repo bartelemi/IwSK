0882601c7d8a12fb4d02905b6af3fe8f8534363e
Szymon Bartnik <szymbar099@student.polsl.pl>
2015-09-06 17:24:22 +0200

    Added missing files.

---
+++ RS485/RS485.Common/Interfaces/IModbusMaster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using RS485.Common.GuiCommon.Models.EventArgs;
+using RS485.Common.Model;
+
+namespace RS485.Common.Interfaces
+{
+    public delegate void FirstCommandCompleted(CommandResult result);
+
+    public delegate void SecondCommandCompleted(CommandResult result, String resultMessage);
+
+    public interface IModbusMaster
+    {
+        event FirstCommandCompleted FirstCommandCompletedHandlers;
+        event SecondCommandCompleted SecondCommandCompletedHandlers;
+        event LogMessageOccuredEventHandler LogMessageOccured;
+
+        void SetConnectionSettings(ConnectionSettings settings);
+
+        void SetModbusSettings(ModbusSettings settings);
+
+        void SendFirstCommand(String slaveAddress, String message);
+
+        void SendSecondCommand(String slaveAddress);
+    }
+}
